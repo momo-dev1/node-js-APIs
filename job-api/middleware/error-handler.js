81aa329ad15a3a97f40dbbcdac85f8e85a3742c4
momo-dev1 <didy1234567@gmail.com>
2022-07-17 23:08:34 +0200

    handling errors

--- job-api/middleware/error-handler.js
+++ job-api/middleware/error-handler.js
@@ -1,10 +1,22 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong',
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message })
+  }
+  if (err.code && err.code === '11000') {
+    customError.message = `this ${ Object.keys(err.keyValue) } already exists please try another one`
+    customError.statusCode = 400
+  }
+  if (err.name === 'ValidationError') {
+    customError.message = object.values(err.errors).map(e => e.message).join('& ')
+  }
+  return res.status(customError.statusCode).json({ message: customError.message })
+
 }
 
 module.exports = errorHandlerMiddleware
