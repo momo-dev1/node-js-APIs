1f3cf8e03f9f40895c040ec10899fd3c152af55b
momo-dev1 <didy1234567@gmail.com>
2022-07-18 19:58:38 +0200

    CRUD

--- job-api/controllers/jobs.js
+++ job-api/controllers/jobs.js
@@ -8,7 +8,8 @@ const Job = require("../models/job")
     @access  Public
 */
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt")
+    const { userId } = req.user
+    const jobs = await Job.find({ createdBy: userId }).sort("createdAt")
     res.status(StatusCodes.OK).json({ jobs, job_Counts: jobs.length })
 }
 
@@ -34,8 +35,8 @@ const getJob = async (req, res) => {
     @access  Private
 */
 const createJob = async (req, res) => {
-
     req.body.createdBy = req.user.userId
+
     const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({ job })
 }
