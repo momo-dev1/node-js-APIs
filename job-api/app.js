2c5e4bc6f836115cfcdece7397dac8605abcc444
momo-dev1 <57541594+momo-dev1@users.noreply.github.com>
2022-07-19 20:19:05 +0200

    adding swagger

--- job-api/app.js
+++ job-api/app.js
@@ -8,6 +8,11 @@ const connectDB = require('./db/connect');
 
 const authMiddleware = require('./middleware/authentication');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 //routers
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
@@ -16,8 +21,30 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
 // extra packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get('/', (req, res) => {
+  res.send('<h5 style="font-size:30px;">Jobs API</h5><a href="/docs">Documentation</a>');
+})
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter)
